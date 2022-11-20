2b63acb84cdb255288fe87efe1d61a33c027a33c
Ender <33337309+e3ndr@users.noreply.github.com>
2022-11-20 12:22:51 -0600

    CLI: Some directories :^)

--- cli/src/main/java/xyz/e3ndr/aion/Aion.java
+++ cli/src/main/java/xyz/e3ndr/aion/Aion.java
@@ -12,7 +12,14 @@ import xyz.e3ndr.aion.types.AionSourceList;
 import xyz.e3ndr.fastloggingframework.logging.FastLogger;
 
 public class Aion {
-    public static final File BASE_DIR = new File(System.getProperty("aion.basedir", "."));
+    // @formatter:off
+    public static final File BASE_DIR     = new File(System.getProperty("aion.basedir", "."));
+    
+    public static final File DOWNLOAD_DIR = new File(BASE_DIR, "download-cache");
+    public static final File PACKAGES_DIR = new File(BASE_DIR, "packages");
+    public static final File PATH_DIR     = new File(BASE_DIR, "path");
+    // @formatter:on
+
     public static final FastLogger LOGGER = new FastLogger("Aion");
 
     private static Config _config;
@@ -20,7 +27,10 @@ public class Aion {
     private static List<AionPackage.Version> _installCache;
 
     public static void setup() {
-        // :^)
+        DOWNLOAD_DIR.mkdirs();
+        PACKAGES_DIR.mkdirs();
+        PATH_DIR.mkdirs();
+
     }
 
     // Getters, we want to load these things on-demand.
@@ -29,7 +39,6 @@ public class Aion {
         if (_config == null) {
             _config = Config.load();
         }
-
         return _config;
     }
 
@@ -37,7 +46,6 @@ public class Aion {
         if (_sourceCache == null) {
             _sourceCache = Sources.load();
         }
-
         return _sourceCache;
     }
 
@@ -45,7 +53,6 @@ public class Aion {
         if (_installCache == null) {
             _installCache = Installed.load();
         }
-
         return _installCache;
     }
 

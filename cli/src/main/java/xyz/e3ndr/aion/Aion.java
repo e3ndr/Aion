c1c6cecf898af415c9c3d3e4b9a6b2301978947b
Ender <33337309+e3ndr@users.noreply.github.com>
2022-11-20 10:47:52 -0600

    CLI: Load the config/cache files on demand.
    We don't need to load and parse the full config/cache files just to print the help message...

--- cli/src/main/java/xyz/e3ndr/aion/Aion.java
+++ cli/src/main/java/xyz/e3ndr/aion/Aion.java
@@ -2,25 +2,47 @@ package xyz.e3ndr.aion;
 
 import java.util.List;
 
-import lombok.Getter;
 import xyz.e3ndr.aion.configuration.Config;
 import xyz.e3ndr.aion.configuration.Installed;
 import xyz.e3ndr.aion.configuration.Sources;
 import xyz.e3ndr.aion.types.AionPackage;
+import xyz.e3ndr.aion.types.AionPackage.Version;
 import xyz.e3ndr.aion.types.AionSourceList;
 import xyz.e3ndr.fastloggingframework.logging.FastLogger;
 
 public class Aion {
     public static final FastLogger LOGGER = new FastLogger("Aion");
 
-    private static @Getter Config config;
-    private static @Getter List<AionSourceList> sourceCache;
-    private static @Getter List<AionPackage.Version> installCache;
+    private static Config config;
+    private static List<AionSourceList> sourceCache;
+    private static List<AionPackage.Version> installCache;
 
-    public static void setup() {
-        config = Config.load();
-        sourceCache = Sources.load();
-        installCache = Installed.load();
+    public static void setup() {}
+
+    // Getters, we want to load these things on-demand.
+
+    public static Config getConfig() {
+        if (config == null) {
+            config = Config.load();
+        }
+
+        return config;
+    }
+
+    public static List<AionSourceList> getSourceCache() {
+        if (sourceCache == null) {
+            sourceCache = Sources.load();
+        }
+
+        return sourceCache;
+    }
+
+    public static List<Version> getInstallCache() {
+        if (installCache == null) {
+            installCache = Installed.load();
+        }
+
+        return installCache;
     }
 
 }

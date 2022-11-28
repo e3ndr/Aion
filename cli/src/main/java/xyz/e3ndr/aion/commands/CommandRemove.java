fb44663e737232397cb14698e7e62140725bf44f
Ender <33337309+e3ndr@users.noreply.github.com>
2022-11-27 22:37:05 -0600

    CLI: The `remove` command.

--- cli/src/main/java/xyz/e3ndr/aion/commands/CommandRemove.java
+++ cli/src/main/java/xyz/e3ndr/aion/commands/CommandRemove.java
@@ -1,11 +1,22 @@
 package xyz.e3ndr.aion.commands;
 
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import co.casterlabs.commons.functional.tuples.Pair;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 import xyz.e3ndr.aion.Aion;
+import xyz.e3ndr.aion.UserInput;
+import xyz.e3ndr.aion.Util;
+import xyz.e3ndr.aion.configuration.Installed;
+import xyz.e3ndr.aion.configuration.Installed.InstallCacheEntry;
+import xyz.e3ndr.aion.types.AionPackage;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -29,8 +40,80 @@ public class CommandRemove implements Runnable {
 
     @Override
     public void run() {
-        // TODO
-        Aion.LOGGER.fatal("TODO");
+        List<Pair<String, String>> packagesToFind = AionCommands.parseAllVersions(this.interim_packagesToRemove);
+
+        // Look for the packages in the source cache.
+        Aion.LOGGER.info("Looking for packages...");
+
+        List<AionPackage.Version> packages = Aion
+            .installCache()
+            .parallelStream()
+            .map((entry) -> entry.pkg.getVersions().get(entry.version))
+            .filter((version) -> {
+                for (Pair<String, String> toFind : packagesToFind) {
+                    if (version.getPkg().getSlug().equals(toFind.a()) &&
+                        version.getVersion().equals(toFind.b())) {
+                        return true;
+                    }
+                }
+
+                return false;
+            })
+            .collect(Collectors.toList());
+
+        if (packages.size() == 0) {
+            Aion.LOGGER.info("No packages will be removed.");
+            return;
+        } else {
+            Aion.LOGGER.info("The following packages will be removed:");
+            for (AionPackage.Version version : packages) {
+                Aion.LOGGER.info("    %s:%s (%s) %s", version.getPkg().getSlug(), version.getVersion(), version.getPatch(), version.getPkg().getAliases());
+            }
+        }
+
+        Aion.LOGGER.info("A total of %d package(s) will be removed.", packages.size());
+
+        // Dry run
+        if (this.isDryRun) {
+            Aion.LOGGER.info("----End of dry run----");
+            return;
+        }
+
+        if (!this.autoYes) {
+            Aion.LOGGER.info("Are you sure you wish to remove the above packages? (Y/n)");
+
+            if (!UserInput.confirm()) {
+                Aion.LOGGER.info("Aborting remove.");
+                return;
+            }
+        }
+
+        for (AionPackage.Version version : packages) {
+            // We already know that `version` is in this list, we're just removing it.
+            Iterator<InstallCacheEntry> it = Aion.installCache().iterator();
+            while (it.hasNext()) {
+                InstallCacheEntry entry = it.next();
+                if (entry.pkg.getSlug().equals(version.getPkg().getSlug()) &&
+                    entry.version.equals(version.getVersion())) {
+                    it.remove();
+                    break;
+                }
+            }
+
+            Installed.save(Aion.installCache());
+
+            // Delete the dir.
+            File packageWorkingDir = new File(
+                Aion.PACKAGES_DIR,
+                String.format("%s/%s", version.getPkg().getSlug(), version.getVersion())
+            );
+
+            Util.recursivelyDeleteDirectory(packageWorkingDir);
+        }
+
+        // TODO find orphaned dependencies and recursively remove them.
+
+        AionCommands.path_rebuild();
     }
 
 }
